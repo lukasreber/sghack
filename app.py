9ed2e888fc30c061818f3bc2fec7cc68013a5f26
Lukas Reber <lukasreber@lukr.ch>
2021-11-06 11:42:59 +0100

    compare uni and supermarket search

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import folium
 st.image('logo.jpeg')
 
 # Import all necessary dat
+immo = pd.read_csv('immo_uni.csv')
 immo_uni = pd.read_csv('immo_uni.csv')
+immo_supermarket = pd.read_csv('supermarkets.csv')
 
 
 st.sidebar.title("Find Your Flat")
@@ -15,10 +17,19 @@ st.sidebar.image('opendata_sg.png')
 
 # Filter for Distance to Universities
 dist_uni = st.slider('Wähle die maximale Laufdistanz in Meter zur Uni',0,6000)
-immo_uni = immo_uni[immo_uni.distance_meters <= dist_uni]
+immo_uni = immo_uni[immo_uni.distance_meters <= dist_uni].id.tolist()
+
+# Filter for Distance to Supermarkets
+dist_supermarket = st.slider('Wähle die maximale Laufdistanz in Meter zum nächsten Supermarkt',0,6000)
+immo_supermarket = immo_supermarket[immo_supermarket.distance_meters <= dist_supermarket].id.tolist()
+
+# compare all lists
+res_id = set(immo_uni) & set(immo_supermarket)
+
+res = immo[immo.id.isin(res_id)]
 
 # Return count of search results
-st.text(f'Deine Suche ergibt {len(immo_uni.index)} Treffer')
+st.text(f'Deine Suche ergibt {len(res.index)} Treffer')
 
 # Display Results on Map
 startloc = ['47.422124', '9.368013']
@@ -35,10 +46,10 @@ m = folium.Map(location=[startloc[0],startloc[1]], zoom_start=13)
 
 #folium.Marker([location[0],location[1]],popup='selection',icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
 
-for index, row in immo_uni.iterrows():
+for index, row in res.iterrows():
     popup = folium.Popup('Adresse: {0}<br>Preis: {1}'.format(row['street_number'],row.rent),max_width=300)
     folium.Marker([row.lat,row.lng],popup=popup).add_to(m)
 
 folium_static(m)
 
-st.dataframe(immo_uni)
+st.dataframe(res)
