bb9e5db7a6e8129b9322c4e56d96a83a1d364fb7
Lukas Reber <lukasreber@lukr.ch>
2021-11-06 11:03:43 +0100

    ui improvements

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ from streamlit_folium import folium_static
 import pandas as pd
 import folium
 
+st.image('logo.jpeg')
+
 # Import all necessary dat
 immo_uni = pd.read_csv('immo_uni.csv')
 
 
-st.title("Find Your Flat")
+st.sidebar.title("Find Your Flat")
+st.sidebar.subheader('Ein Projekt von Niels Ham und Lukas Reber')
+st.sidebar.image('opendata_sg.png')
 
 # Filter for Distance to Universities
 dist_uni = st.slider('Wähle die maximale Laufdistanz in Meter zur Uni',0,6000)
 immo_uni = immo_uni[immo_uni.distance_meters <= dist_uni]
+
+# Return count of search results
+st.text(f'Deine Suche ergibt {len(immo_uni.index)} Treffer')
 
 # Display Results on Map
 startloc = ['47.422124', '9.368013']
@@ -29,7 +36,8 @@ m = folium.Map(location=[startloc[0],startloc[1]], zoom_start=13)
 #folium.Marker([location[0],location[1]],popup='selection',icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
 
 for index, row in immo_uni.iterrows():
-    folium.Marker([row.lat,row.lng],popup=row['street_number']).add_to(m)
+    popup = folium.Popup('Adresse: {0}<br>Preis: {1}'.format(row['street_number'],row.rent),max_width=300)
+    folium.Marker([row.lat,row.lng],popup=popup).add_to(m)
 
 folium_static(m)
 
