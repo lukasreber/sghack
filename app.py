ae799b7c071c9d5e4ec040f9baee6f0dc41512df
Lukas Reber <lukasreber@lukr.ch>
2021-11-06 09:16:32 +0100

    search field uni implemented

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 import streamlit as st
 from streamlit_folium import folium_static
 import pandas as pd
-import requests
 import folium
 
-st.title("ImmoFinder")
+# Import all necessary dat
+immo_uni = pd.read_csv('immo_uni.csv')
 
-immo = pd.read_csv('https://raw.githubusercontent.com/cividi/st-gallen-urban-indicators/main/data/price-monitoring/price-monitoring.csv')
 
-st.dataframe(immo)
+st.title("Find Your Flat")
+
+# Filter for Distance to Universities
+dist_uni = st.slider('Wähle die maximale Laufdistanz in Meter zur Uni',0,6000)
+immo_uni = immo_uni[immo_uni.distance_meters <= dist_uni]
+
+# Display Results on Map
+startloc = ['47.422124', '9.368013']
+m = folium.Map(location=[startloc[0],startloc[1]], zoom_start=13)
+
+# folium.Circle(
+#     location=[location[0],location[1]], 
+#     radius=radius,
+#     color="#3186cc",
+#     fill=True,
+#     fill_color="#3186cc",
+#     popup="test"
+# ).add_to(m)
+
+#folium.Marker([location[0],location[1]],popup='selection',icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
+
+for index, row in immo_uni.iterrows():
+    folium.Marker([row.lat,row.lng],popup=row['street_number']).add_to(m)
+
+folium_static(m)
+
+st.dataframe(immo_uni)
